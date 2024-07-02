668fbb9f979ee583eb15f10c629324d928fa8213
Tyler Sloan <tcsloan44@gmail.com>
2024-07-01 23:23:46 -0400

    added api files

--- db/db.js
+++ db/db.js
@@ -2,4 +2,14 @@ import pg from 'pg'
 
 const { Client } = pg;
 
-const DATABASE_URL = process.env.DATABASE_URL || "postgres://localhost:3001"
+const DATABASE_URL = process.env.DATABASE_URL || "postgres://localhost:3001/book_fair"
+
+export const client = new Client(DATABASE_URL);
+
+export const connectDB = async () => {
+    try{
+
+    }catch(e){
+        console.error(`Failure to connect to ${DATABASE_URL}`)
+    }
+}
