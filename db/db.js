ec912b8ed86529b18eafdb4231f0db99a2438963
Tyler Sloan <tcsloan44@gmail.com>
2024-07-02 23:20:55 -0400

    modified seed.js to have initialize the database table

--- db/db.js
+++ db/db.js
@@ -2,9 +2,9 @@ import pg from 'pg'
 
 const { Client } = pg;
 
-const DATABASE_URL = process.env.DATABASE_URL || "postgres://localhost:3001/book_fair"
+const DATABASE_URL = process.env.DATABASE_URL || "postgres://localhost:5432/book_fair"
 
-export const client = new Client(DATABASE_URL);
+const client = new Client(DATABASE_URL);
 
 export const connectDB = async () => {
     try{
@@ -17,3 +17,5 @@ export const connectDB = async () => {
         console.error(`Failure to connect to ${DATABASE_URL}`)
     }
 }
+
+export default client;
---
+++ db/seed.js
@@ -0,0 +1,90 @@
+import client from "./db.js"
+import { connectDB } from "./db.js"
+
+const dropTables = async()=>{
+    try{
+        await client.query(`DROP TABLE IF EXISTS users CASCADE`)
+        await client.query(`DROP TABLE IF EXISTS products CASCADE`)
+        await client.query(`DROP TABLE IF EXISTS reviews CASCADE`)
+        await client.query(`DROP TABLE IF EXISTS cart CASCADE`)
+        await client.query(`DROP TABLE IF EXISTS orders CASCADE`)
+        await client.query(`DROP TABLE IF EXISTS orderItems CASCADE`)
+    }catch(e){
+        console.log(e);
+    }
+}
+
+
+const createTables = async ()=>{
+    try{
+        await client.query(`
+            CREATE TABLE IF NOT EXISTS users(
+                id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+                username VARCHAR(255),
+                password VARCHAR(255)
+            )
+        `)
+        await client.query(`
+            CREATE TABLE IF NOT EXISTS products(
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(255) NOT NULL,
+                price MONEY NOT NULL,
+                details VARCHAR(255),
+                inStock BOOLEAN,
+                quantity INTEGER NOT NULL
+            )
+        `)
+        await client.query(`
+            CREATE TABLE IF NOT EXISTS reviews(
+                id SERIAL PRIMARY KEY,
+                products_id INTEGER REFERENCES products(id) NOT NULL,
+                users_id uuid REFERENCES users(id) NOT NULL,
+                rating INTEGER CHECK (rating BETWEEN 0 and 6),
+                comments VARCHAR(400)
+            )
+        `)
+        await client.query(`
+            CREATE TABLE IF NOT EXISTS cart(
+                id SERIAL PRIMARY KEY,
+                products_id INTEGER REFERENCES products(id) NOT NULL,
+                users_id uuid REFERENCES users(id),
+                quantity INTEGER CHECK (quantity BETWEEN 0 and 100),
+                price MONEY NOT NULL
+            )
+        `)
+        await client.query(`
+            CREATE TABLE IF NOT EXISTS orders(
+                id SERIAL PRIMARY KEY,
+                users_id uuid REFERENCES users(id) NOT NULL,
+                products_id INTEGER REFERENCES products(id) NOT NULL,
+                orderTotal MONEY NOT NULL,
+                orderDate TIMESTAMP DEFAULT now(),
+                productPrice MONEY NOT NULL,
+                quantity INTEGER CHECK (quantity BETWEEN 0 and 1000)
+            )
+        `)
+        await client.query(`
+            CREATE TABLE IF NOT EXISTS orderItems(
+                id SERIAL PRIMARY KEY,
+                products_id INTEGER REFERENCES products(id) NOT NULL,
+                quantity INTEGER CHECK (quantity BETWEEN 0 and 1000),
+                price MONEY NOT NULL
+            )
+        `)
+    }catch(e){
+        console.error("Failure to seed", e)
+    }
+}
+
+const rebuildDB = async()=>{
+    try{
+        await connectDB();
+        await dropTables();
+        await createTables();
+    }catch(e){
+        console.error('Failed to rebuild database')
+    }finally{
+        client.end();
+    }
+}
+rebuildDB();
